d3c06c54b0d5d557bdb7ba9f305bf168b3128dfd
Cole Rau <colevrau@gmail.com>
2019-10-21 00:35:58 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -40,6 +40,8 @@ def matrix_lookup(matrix, row, column)
 end
 
 def matrix_update(matrix, row, column, new_value)
+  
+  
   [ENNET_HOUSE, ENFIELD_TENNIS_ACADEMY][row][column] = new_value
   [ENNET_HOUSE, ENFIELD_TENNIS_ACADEMY]
 end
